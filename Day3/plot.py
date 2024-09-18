4bf63fc6f4c29ce3dc5747ade8f1f529e2188611
lebigdbiengras <dariusnallet@gmail.com>
2024-09-18 17:56:18 +0200

    image of cos sin tan

---
+++ Day3/plot.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.linspace(0,10)
+sin = np.sin(x)
+tan = np.tan(x)
+cos = np.cos(x)
+
+
+plt.figure(figsize=(15,10))
+plt.subplot(2,3,1)
+plt.plot(x,sin,color='blue')
+plt.xlabel('x')
+plt.ylabel('sinus')
+
+
+plt.subplot(2,3,3)
+plt.plot(x,cos,color='red')
+plt.xlabel('x')
+plt.ylabel('cosinus')
+plt.grid=True
+
+
+plt.subplot(2,3,5)
+plt.plot(x,tan,color='green')
+plt.xlabel('x')
+plt.ylabel('tan')
+
+plt.savefig("figure.png")
